7baca57f6e8e1541135c0b03b4114f9fd43f8698
Tomas <tomaslq98@gmail.com>
2024-12-22 03:25:05 -0300

    update: validaciones de input ruta de aprendizaje

--- src/middlewares/validators/learningPathValidators.ts
+++ src/middlewares/validators/learningPathValidators.ts
@@ -3,6 +3,6 @@ import { check } from "express-validator";
 
 export const validateLearningPathInput = [
     check('input').exists().withMessage('').isString().withMessage('La entrada debe ser un texto').escape()
-    .matches(/^[A-Za-záéíóúÁÉÍÓÚñÑ\d\s]+$/).withMessage('Solo se permiten caracteres y números')
-    .isLength({ max: 60 }).withMessage('La entrada debe ser menor a 100 caracteres'),
+    .matches(/^[A-Za-záéíóúÁÉÍÓÚñÑ.#!$\d\s]+$/).withMessage('Solo se permiten caracteres, números y #, !, $, .')
+    .isLength({ max: 30 }).withMessage('La entrada debe ser menor a 30 caracteres'),
 ];
